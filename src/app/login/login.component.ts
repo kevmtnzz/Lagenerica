d0784731c2909133978c908a2384bf57692bc660
kevmtnzz <kev.mtnzz@gmail.com>
2021-11-08 09:04:30 -0500

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+      //login
+    //lista_users=["user1","user2","user3"];
+
+    user_correcto:string="admininicial";
+    pass_correcto:string="admin123456";
+  
+    user:string="";
+    pass:string="";
+  
+    correcto:number=-1;
+
+    comparar() {
+      if (this.user === this.user_correcto){
+        this.correcto = 1;
+        if (this.pass === this.pass_correcto){
+          this.correcto = 1;
+        } else {
+          this.correcto = 0;
+        }
+      } else {
+        this.correcto = 0;
+      }
+    }
+}
